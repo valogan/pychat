05ec00026123757976f1077517dea9f1f30b1dbd
Vaiden Logan <william.logan1@uky.edu>
2024-02-16 13:37:08 -0500

    add the basic code

---
+++ chatServer.py
@@ -0,0 +1,93 @@
+from socket import *
+import threading
+
+SIZE = 2048
+connections = []
+
+class User:
+    def __init__(self, conn, addr):
+        self.conn = conn
+        self.addr = addr
+    def __str__(self):
+        return str(self.addr) + " " + str(self.name)
+    name = None
+    channel = None
+    connectionStatus = True
+        
+
+def doCommand(message, user):
+    message = message.split(" ")
+    if message[0] == "/NAME":
+        user.name = message[1].replace("\n","")
+    elif message[0] == "/DISCONNECT":
+        user.conn.close()
+        print(f"[DISCONNECTION] {user} disconnected")
+        connections.remove(user)
+        user.connectionStatus = False
+    elif message[0] == "/MSG":
+        singlecast(message[1], message[2:], user)
+    else:
+        user.conn.send("command not found\n".encode())
+        
+    
+def broadcast(message, user):
+    for users in connections:
+        if users.addr != user.addr:
+            toSend = user.name + ": " + message + "\n"
+            users.conn.send(toSend.encode())
+
+
+def singlecast(name, message, user):
+    if user.name:
+        for users in connections:
+            if name == users.name:
+                finalMessage = ""
+                for i in message:
+                    finalMessage = finalMessage + i + " "
+                finalMessage = finalMessage[:-1]
+                finalMessage = user.name + " (private message): " + finalMessage + "\n"
+                users.conn.send(finalMessage.encode())
+                return
+        user.conn.send(f"{name} not found\n".encode())
+    else:
+        user.conn.send("Please pick a name using \"/NAME\" command\n".encode())
+    return
+
+        
+def handle_client(user):
+    print(f"[NEW CONNECTION] {user.addr} connected.")
+    user.conn.send("Welcome to the server!\n".encode())
+    while True:
+        data = user.conn.recv(SIZE).decode()
+        data = data.replace("\r", "")
+        data = data.replace("\n", "")
+        print(f"{user}: {data}")
+        if data[0] == "/":
+            doCommand(data, user)
+            if not user.connectionStatus:
+                return
+        else:
+            if user.name:
+                broadcast(data, user)
+            else:
+                user.conn.send("Please pick a name using \"/NAME\" command\n".encode())
+
+                
+def main():
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    IP = ""
+    PORT = 6789
+    ADDR = (IP, PORT)
+    print("Server binding...")
+    serverSocket.bind(ADDR)
+    serverSocket.listen()
+    print(f"Server listening on {ADDR}")
+    while True:
+        conn, addr = serverSocket.accept()
+        user = User(conn, addr)
+        connections.append(user)
+        thread = threading.Thread(target = handle_client, args = (user,))
+        thread.start()
+
+if __name__ == "__main__":
+    main()
